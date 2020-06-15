37c82523c1ce9c0f39da3f4d57cc5d8d5eca000f
ernie-ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-06-15 15:18:11 -0400

    TShirt and Job Role functionality
    has been created. Must pick up again with Activities.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # interactive-form-v2
  Unit 3 Project--Interactive Form
+
+# Development Journal
+- Day 0:
+  I'm going to begin development with Unobtrusive JS in mind, meaning designing and developing the form to work perfecting and comfortable with JS disabled. This is because JS (as powerful as it is) seems to be a common single point of failure.
+
+  Here's a thought, how about using CSS to hide and show T-Shirt Info options instead of JS. This may be out of the scope of this project...
+  Yup, it is. Don't get caught up on a design feature for bragging right. It's too early for that.
+
+  Progressive enhancement reqs:
+    The “Other” text field under the "Job Role" section should be visible
+    All information for Bitcoin, PayPal or Credit Card payments should be visible.
+
+  Encouraged CSS 'sugar':
+    color,
+    background color,
+    font,
+    transitions,
+    animations,
+    box shadows,
+    text shadows
+
+  CSS goal for this project:
+    Night/Day Mode themes. Yes!
+
+
+# Todo:
+- css/reset.css or css/normalize.css - This is not a project requirement, but using a reset or normalize CSS file is an important common standard in web development as it helps zero out the initial styles that different browsers add to their page by default. So you are encouraged to take this extra step. You should be able to find what you need with a Google search.
--- js/script.js
+++ js/script.js
@@ -6,17 +6,75 @@ name.focus();
 // Target the ‘Other’ input field, and hide it initially, so that it will
 // display if JavaScript is disabled, but be hidden initially with JS.
 const otherJobRole = document.getElementById('other-title');
-otherJobRole.style.display = 'none';
+let jobRoleInput = otherJobRole.style;
+jobRoleInput.display = 'none';
 
-// When the form is initially loaded, we need to update the "Design" and "Color" fields so that it's
-// clear to the user that they need to select a theme before selecting a color.
+// Text field will be revealed only when the "Other" option is selected
+// from the "Job Role" drop down menu.
+const otherJobRoleDisplay = document.querySelector('#title');
+otherJobRoleDisplay.addEventListener('change', (e) => {
+  const ifSelectedOption = e.target.value;
+
+  ifSelectedOption === 'other' ? jobRoleInput.display = 'block' : jobRoleInput.display = 'none'
+})
 
 // Hide the “Select Theme” `option` element in the “Design” menu.
 const colors = document.getElementById('colors-js-puns');
 colors.style.display = 'none';
 
+// gets passed the value of one of the options from the design menu
+const handleDesignTheme = (theme, selectHTML) => {
+  if (theme === 'js puns') {
+    // if 'js puns' gets passed, it displays Color menu with cornflowerblue,
+    //  darkslategrey, and gold options
+    const punsHTML = `<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
+    <option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>
+    <option value="gold">Gold (JS Puns shirt only)</option>`
+    selectHTML.innerHTML = punsHTML;
+    colors.style.display = 'block';
+  } else if (theme === 'heart js') {
+    // if 'heart js' gets passed, it displays Color menu with tomato,
+    //  steelblue, and dimgrey options
+    const heartHTML = `<option value="tomato">Tomato (I &#9829; JS shirt only)</option>
+    <option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option>
+    <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>`
+    selectHTML.innerHTML = heartHTML;
+    colors.style.display = 'block';
+  } else {
+    // if 'Select Theme' gets passed, it displays Color menu with one
+    //  option that says 'Please select a T-shirt theme'
+    const selectThemeHTML = `<option value="selectTheme" disable="true">Please select a T-shirt theme</option>`
+    selectHTML.innerHTML = selectThemeHTML;
+    colors.style.display = 'block';
+  }
+}
+
 const design = document.getElementById('design');
-design.addEventListener('click', (e) => {
-  const options = document.querySelectorAll('#design option');
-  console.log(options);
+design.addEventListener('change', (e) => {
+    const theme = e.target.value;
+    const selectHTML = document.querySelector('#color');
+    handleDesignTheme(theme, selectHTML);
+  })
+
+/*
+Some events are at the same day and time as others.
+*/
+const activities = document.querySelector('.activities');
+const checkboxes = activities.querySelectorAll('input')
+console.log(checkboxes);
+activities.addEventListener('change', (e) => {
+  console.log(e.target.checked);
 })
+/*
+If the user selects a workshop, don't allow selection of a workshop at the same day and time
+    -- you should disable the checkbox and
+       visually indicate that the workshop in the competing time slot isn't available.
+*/
+
+/*
+When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
+*/
+
+/*
+As a user selects activities, a running total should display below the list of checkboxes. For example, if the user selects "Main Conference", then Total: $200 should appear. If they add 1 workshop, the total should change to Total: $300.
+*/
