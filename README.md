d7923e91e0cf1addef5758b3fe5e19281bc80b40
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-06-20 16:24:27 -0400

    Ready for QA
    Finished all functionality

--- README.md
+++ README.md
@@ -1,28 +1,8 @@
 # interactive-form-v2
  Unit 3 Project--Interactive Form
 
-# Development Journal
-- Day 0:
-  I'm going to begin development with Unobtrusive JS in mind, meaning designing and developing the form to work perfecting and comfortable with JS disabled. This is because JS (as powerful as it is) seems to be a common single point of failure.
-
-  Here's a thought, how about using CSS to hide and show T-Shirt Info options instead of JS. This may be out of the scope of this project...
-  Yup, it is. Don't get caught up on a design feature for bragging right. It's too early for that.
-
-  Progressive enhancement reqs:
-    The “Other” text field under the "Job Role" section should be visible
-    All information for Bitcoin, PayPal or Credit Card payments should be visible.
-
-  Encouraged CSS 'sugar':
-    color,
-    background color,
-    font,
-    transitions,
-    animations,
-    box shadows,
-    text shadows
-
-  CSS goal for this project:
-    Night/Day Mode themes. Yes!
+# Real-time Error Messages
+Rather than providing an error message on submit, the form checks for errors and display messages as the user begins typing inside a text field. The error appears as the user begins to type, and disappears as soon as the user has entered a complete and correctly formatted input.
 
 
 # Todo:
--- js/script.js
+++ js/script.js
@@ -1,117 +1,122 @@
-
 // Use JavaScript to select the 'Name' input element and place focus on it.
-const name = document.getElementById('name');
+const name = document.getElementById("name");
 name.focus();
 
 // Target the ‘Other’ input field, and hide it initially, so that it will
 // display if JavaScript is disabled, but be hidden initially with JS.
-const otherJobRole = document.getElementById('other-title');
+const otherJobRole = document.getElementById("other-title");
 let jobRoleInput = otherJobRole.style;
-jobRoleInput.display = 'none';
+jobRoleInput.display = "none";
 
 // Text field will be revealed only when the "Other" option is selected
 // from the "Job Role" drop down menu.
-const otherJobRoleDisplay = document.querySelector('#title');
-otherJobRoleDisplay.addEventListener('change', (e) => {
+const otherJobRoleDisplay = document.querySelector("#title");
+otherJobRoleDisplay.addEventListener("change", (e) => {
   const ifSelectedOption = e.target.value;
 
-  ifSelectedOption === 'other' ? jobRoleInput.display = 'block' : jobRoleInput.display = 'none'
-})
+  ifSelectedOption === "other"
+    ? (jobRoleInput.display = "block")
+    : (jobRoleInput.display = "none");
+});
 
 // Hide the “Select Theme” `option` element in the “Design” menu.
-const colors = document.getElementById('colors-js-puns');
-colors.style.display = 'none';
+const colors = document.getElementById("colors-js-puns");
+colors.style.display = "none";
 
 // gets passed the value of one of the options from the design menu
 const handleDesignTheme = (theme, selectHTML) => {
-  if (theme === 'js puns') {
+  if (theme === "js puns") {
     // if 'js puns' gets passed, it displays Color menu with cornflowerblue,
     //  darkslategrey, and gold options
     const punsHTML = `<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
     <option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>
-    <option value="gold">Gold (JS Puns shirt only)</option>`
+    <option value="gold">Gold (JS Puns shirt only)</option>`;
     selectHTML.innerHTML = punsHTML;
-    colors.style.display = 'block';
-  } else if (theme === 'heart js') {
+    colors.style.display = "block";
+  } else if (theme === "heart js") {
     // if 'heart js' gets passed, it displays Color menu with tomato,
     //  steelblue, and dimgrey options
     const heartHTML = `<option value="tomato">Tomato (I &#9829; JS shirt only)</option>
     <option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option>
-    <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>`
+    <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>`;
     selectHTML.innerHTML = heartHTML;
-    colors.style.display = 'block';
+    colors.style.display = "block";
   } else {
     // if 'Select Theme' gets passed, it displays Color menu with one
     //  option that says 'Please select a T-shirt theme'
-    const selectThemeHTML = `<option value="selectTheme" disable="true">Please select a T-shirt theme</option>`
+    const selectThemeHTML = `<option value="selectTheme" disable="true">Please select a T-shirt theme</option>`;
     selectHTML.innerHTML = selectThemeHTML;
-    colors.style.display = 'block';
+    colors.style.display = "block";
   }
-}
+};
 
-const design = document.getElementById('design');
-design.addEventListener('change', (e) => {
-    const theme = e.target.value;
-    const selectHTML = document.querySelector('#color');
-    handleDesignTheme(theme, selectHTML);
-  })
+const design = document.getElementById("design");
+design.addEventListener("change", (e) => {
+  const theme = e.target.value;
+  const selectHTML = document.querySelector("#color");
+  handleDesignTheme(theme, selectHTML);
+});
 
 const handleActivities = () => {
-  const activities = document.querySelector('.activities');
-  const checkboxes = activities.querySelectorAll('input')
+  const activities = document.querySelector(".activities");
+  const checkboxes = activities.querySelectorAll("input");
   let isChecked = [];
   for (var i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) { isChecked.push(checkboxes[i]) }
+    if (checkboxes[i].checked) {
+      isChecked.push(checkboxes[i]);
+    }
   }
-  return isChecked
-}
+  return isChecked;
+};
 
 const createEl = (el, id, text) => {
   const elem = document.createElement(el);
   elem.id = id;
   elem.textContent = text;
-  return elem
-}
+  return elem;
+};
 
 const removePreviousPrice = () => {
-  if (document.querySelector('#price')) {
-    const parent = document.querySelector('#price').parentNode;
-    const child = document.querySelector('#price');
+  if (document.querySelector("#price")) {
+    const parent = document.querySelector("#price").parentNode;
+    const child = document.querySelector("#price");
     parent.removeChild(child);
   }
-}
+};
 
+let activitySelected = false;
 const setNewPrice = () => {
   const isChecked = handleActivities();
   const activityCosts = [];
   for (let i = 0; i < isChecked.length; i++) {
     activityCosts.push(parseInt(isChecked[i].dataset.cost));
   }
-  const totalCost = activityCosts.reduce(function(a, b){
-      return a + b;
+  const totalCost = activityCosts.reduce(function (a, b) {
+    return a + b;
   }, 0); //https://www.tutorialrepublic.com/faq/how-to-find-the-sum-of-an-array-of-numbers-in-javascript.php
   if (totalCost === 0) {
-    const text = `Total Price:`
-    const priceHTML =  createEl('label', "price", text);
-    activities.appendChild(priceHTML)
+    const text = `Total Price: 🤪 Must Select At Least One Activity`;
+    const priceHTML = createEl("label", "price", text);
+    activities.appendChild(priceHTML);
+    activitySelected = false;
   } else {
-    const text = `Total Price: $${totalCost}`
-    const priceHTML =  createEl('label', "price", text);
-    activities.appendChild(priceHTML)
+    const text = `Total Price: $${totalCost}`;
+    const priceHTML = createEl("label", "price", text);
+    activities.appendChild(priceHTML);
+    activitySelected = true;
   }
-}
+};
 
 const handleTotalPrice = () => {
   removePreviousPrice();
   setNewPrice();
-}
-
+};
 
 /*
 Some events are at the same day and time as others.
 */
-const activities = document.querySelector('.activities');
-const checkboxes = activities.querySelectorAll('input')
+const activities = document.querySelector(".activities");
+const checkboxes = activities.querySelectorAll("input");
 
 const handleScheduling = (e) => {
   const dayAndTime = e.target.dataset.dayAndTime;
@@ -130,152 +135,243 @@ const handleScheduling = (e) => {
   const node = checkboxes[4];
   const dayAndTimeNode = node.dataset.dayAndTime;
 
-  if ( nameOfChecked === 'js-frameworks' && dayAndTime === dayAndTimeExpress ) {
+  if (nameOfChecked === "js-frameworks" && dayAndTime === dayAndTimeExpress) {
     express.disabled = true;
   }
-  if (nameOfChecked === 'js-frameworks' && !targetChecked) {
+  if (nameOfChecked === "js-frameworks" && !targetChecked) {
     express.disabled = false;
   }
-  if ( nameOfChecked === 'js-libs' && dayAndTime === dayAndTimeNode ) {
+  if (nameOfChecked === "js-libs" && dayAndTime === dayAndTimeNode) {
     node.disabled = true;
   }
-  if ( nameOfChecked === 'js-libs' && !targetChecked ) {
+  if (nameOfChecked === "js-libs" && !targetChecked) {
     node.disabled = false;
   }
-  if ( nameOfChecked === 'express' && dayAndTime === dayAndTimeFrameworks ) {
+  if (nameOfChecked === "express" && dayAndTime === dayAndTimeFrameworks) {
     frameworks.disabled = true;
   }
-  if ( nameOfChecked === 'express' && !targetChecked ) {
+  if (nameOfChecked === "express" && !targetChecked) {
     frameworks.disabled = false;
   }
-  if ( nameOfChecked === 'node' && dayAndTime === dayAndTimeLibs ) {
+  if (nameOfChecked === "node" && dayAndTime === dayAndTimeLibs) {
     libraries.disabled = true;
   }
-  if ( nameOfChecked === 'node' && !targetChecked ) {
+  if (nameOfChecked === "node" && !targetChecked) {
     libraries.disabled = false;
   }
-}
+};
 
 // append `Total Price:` to page upon initial load
-const text = `Total Price:`
-const priceHTML =  createEl('label', "price", text);
-activities.appendChild(priceHTML)
+const text = `Total Price:`;
+const priceHTML = createEl("label", "price", text);
+activities.appendChild(priceHTML);
 
 // track what activities have been checked off and total price
-activities.addEventListener('change', (e) => {
+activities.addEventListener("change", (e) => {
   handleScheduling(e);
   handleTotalPrice();
-})
+});
 
 // Make Credit Card the default option by removing "select method" option,
 // and hidding paypal and bitcoin messages
-const payments = document.querySelector('#payment');
-const paymentOptions = payments.querySelectorAll('option');
-payments.removeChild(paymentOptions[0])
-document.querySelector('#paypal').style.display = 'none'
-document.querySelector('#bitcoin').style.display = 'none'
+const payments = document.querySelector("#payment");
+const paymentOptions = payments.querySelectorAll("option");
+payments.removeChild(paymentOptions[0]);
+document.querySelector("#paypal").style.display = "none";
+document.querySelector("#bitcoin").style.display = "none";
 
 // Display payment sections based on the payment option chosen in the select menu.
-payments.addEventListener('change', (e) => {
-  console.log(e.target.value);
-  // The "Credit Card" payment option should be selected by default. Display the #credit-card div, and hide the "PayPal" and "Bitcoin" information. Payment option in the select menu should match the payment option displayed on the page.
-  if (e.target.value === 'credit card') {
-    document.querySelector('#credit-card').style.display = 'block'
-    document.querySelector('#paypal').style.display = 'none'
-    document.querySelector('#bitcoin').style.display = 'none'
+let payWithCredit = true;
+let payWithPaypal = false;
+let payWithBitcoin = false;
+payments.addEventListener("change", (e) => {
+  // Display the #credit-card div, and hide the "PayPal" and "Bitcoin" information. 
+  if (e.target.value === "credit card") {
+    document.querySelector("#credit-card").style.display = "block";
+    document.querySelector("#paypal").style.display = "none";
+    document.querySelector("#bitcoin").style.display = "none";
+    payWithCredit = true;
+    payWithPaypal = false;
+    payWithBitcoin = false;
   }
   // When a user selects the "PayPal" payment option, the PayPal information should display, and the credit card and “Bitcoin” information should be hidden.
-  if (e.target.value === 'paypal') {
-    document.querySelector('#paypal').style.display = 'block'
-    document.querySelector('#credit-card').style.display = 'none'
-    document.querySelector('#bitcoin').style.display = 'none'
+  if (e.target.value === "paypal") {
+    document.querySelector("#paypal").style.display = "block";
+    document.querySelector("#credit-card").style.display = "none";
+    document.querySelector("#bitcoin").style.display = "none";
+    payWithCredit = false;
+    payWithPaypal = true;
+    payWithBitcoin = false;
   }
   // When a user selects the "Bitcoin" payment option, the Bitcoin information should display, and the credit card and “PayPal” information should be hidden.
-  if (e.target.value === 'bitcoin') {
-    document.querySelector('#bitcoin').style.display = 'block'
-    document.querySelector('#paypal').style.display = 'none'
-    document.querySelector('#credit-card').style.display = 'none'
-
+  if (e.target.value === "bitcoin") {
+    document.querySelector("#bitcoin").style.display = "block";
+    document.querySelector("#paypal").style.display = "none";
+    document.querySelector("#credit-card").style.display = "none";
+    payWithCredit = false;
+    payWithPaypal = false;
+    payWithBitcoin = true;
   }
-})
+});
 
-const nameValidation = document.querySelector('[id=name]')
-nameValidation.placeholder = `Full Name`
-nameValidation.addEventListener('change', (e) => {
-  if (nameValidation.value === '') {
-    document.querySelector('[for=name]').textContent = "Name: 🤪"
+// validation for name
+let nameValid = false;
+const nameValidation = document.querySelector("[id=name]");
+nameValidation.placeholder = `X Æ A-ii`;
+nameValidation.addEventListener("change", (e) => {
+  if (nameValidation.value === "") {
+    document.querySelector("[for=name]").textContent = "Name: 🤪 Username must have at least 1 character";
+    nameValid = false;
   } else {
-    document.querySelector('[for=name]').textContent = "Name: 😁"
+    document.querySelector("[for=name]").textContent = "Name: 😁 Nice name!";
+    nameValid = true;
   }
 })
 
-
-/*
-American Express :- Starting with 34 or 37, length 15 digits.
-  3434 123456 12345
-Visa :- Starting with 4, length 13 or 16 digits.
-  4000 1234 1234 1234 or 4234 12345 1234
-MasterCard :- Starting with 51 through 55, length 16 digits.
-  5123 1234 1234 1234
-Discover :- Starting with 6011, length 16 digits or starting with 5, length 15 digits.
-  6011 1234 1234 1234 or 5234 1234 1234 123
-Diners Club :- Starting with 300 through 305, 36, or 38, length 14 digits.
-  3634 123456 1234
-JCB :- Starting with 2131 or 1800, length 15 digits or starting with 35, length 16 digits.
-  3534 1234 1234 1234 or
-
-https://www.w3resource.com/javascript/form/credit-card-validation.php
-*/
-
-// Credit card validation
-const multOddsBy2 = (reversed) => {
-  for (let i = 0; i < reversed.length; i+= 2) {
-    reversed[i] = reversed[i] * 2;
-  }
-  return reversed;
-}
-const sub9IfOver9 = (oddsMultipliedBy2) => {
-  for (let i = 0; i < oddsMultipliedBy2.length; i++) {
-    if (oddsMultipliedBy2[i] > 9) {
-      oddsMultipliedBy2[i] = oddsMultipliedBy2[i] - 9;
+// validation for email
+const emailInput = document.getElementById("mail");
+emailInput.placeholder = `fully@stacked.com`
+let emailValid = false;
+function validationCallback(isValidEmail) {
+  return e => {
+    const formInput = e.target.value;
+    const isValid = isValidEmail(formInput);
+    if (isValid) {
+      document.querySelector("[for=mail]").textContent = `Email: 😁 Nice Email!`;
+      emailValid = true;
+    } else {
+      document.querySelector("[for=mail]").textContent = `Email: 🤪 sorry@try.again`;
+      emailValid = false;
     }
   }
-  return oddsMultipliedBy2;
 }
-const validateCreditCard = (mod10, lastNum) => {
-  if (mod10 === lastNum) {
-    return "Credit Card Number is Valid!";
+
+function isValidEmail(email) {
+  return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
+}
+
+emailInput.addEventListener("input", validationCallback(isValidEmail));
+
+
+
+//validation for credit card number
+const ccNumInput = document.querySelector('#cc-num')
+ccNumInput.placeholder = `0000 0000 0000 0000`
+let creditValid = false;
+function validationCallbackCred(isValidCreditNum) {
+  return e => {
+    const formInput = e.target.value;
+    const isValid = isValidCreditNum(formInput);
+    if (isValid) {
+      document.querySelector("[for=cc-num]").textContent = `Card Number: 😁 Cha-ching!`;
+      creditValid = true;
+    } else {
+      document.querySelector("[for=cc-num]").textContent = `Card Number: 🤪 Card Number must be 13-16 digits`;
+      creditValid = false;
+    }
   }
 }
 
-
-// Luhn algorith for credit card validation
-const origNum = [4, 5, 5,	6, 7,	3, 7, 5, 8, 6, 8, 9, 9, 8, 5, 5];
-
-const creditValidator = (arr) => {
-  return validateCreditCard(sub9IfOver9(multOddsBy2(arr.reverse())).reduce(function(a, b){return a + b; }, 0) % 10, arr.pop());
+function isValidCreditNum(ccNumValue) {
+  return /^\d{13,16}$/.test(ccNumValue);
 }
-//console.log(creditValidator(origNum));
 
-const labelCcNum = document.querySelector('[for=cc-num]');
-
-const ccNum = document.querySelector('[id=cc-num]');
-ccNum.addEventListener('keyup', (e) => {
-  const num = ccNum.value;
-  const numArr = [];
-  for (var i = 0; i < num.length; i++) {
-    numArr.push(parseInt(num[i]));
-  }
-  const firstTwo = parseInt(numArr.splice(0,2).join('')) // use Regex
-  if (firstTwo === 34 || firstTwo === 37) { // use Regex
-    labelCcNum.textContent = 'Card Number: American Express';
-  } else {
-    labelCcNum.textContent = 'Card Number:';
-  }
-
-})
+ccNumInput.addEventListener("input", validationCallbackCred(isValidCreditNum));
 
 /*
-  `\d{13,16}` digits between 13 and 16 numbers in length
-  `\d{4} \d{4} \d{4} \d{4}` matches `1234 1234 1234 1234`
+
+Still considering how to (or whether I should) implement a reformating of the credit card number
+i.e. 0000 0000 0000 0000
+the issue is handling for 13 through 16 digit length combinations.
+
+ccNumInput.addEventListener('input', e => {
+  e.target.value = e.target.value.replace(/[^\d]/g, '').replace(/(.{4})/g, '$1 ').trim();
+}); // https://stackoverflow.com/questions/17260238/how-to-insert-space-every-4-characters-for-iban-registering
+
 */
+
+//validation for zip code
+const zipInput = document.querySelector('#zip')
+zipInput.placeholder = `12345`
+let zipValid = false;
+function validationCallbackZip(isValidZipNum) {
+  return e => {
+    const formInput = e.target.value;
+    const isValid = isValidZipNum(formInput);
+    if (isValid) {
+      document.querySelector("[for=zip]").textContent = `Zip Code: 😁`;
+      zipValid = true;
+    } else {
+      document.querySelector("[for=zip]").textContent = `Zip Code: 🤪 5 digits only`;
+      zipValid = false;
+    }
+  }
+}
+
+function isValidZipNum(zipInput) {
+  return /\d{5}/.test(zipInput);
+}
+
+zipInput.addEventListener("input", validationCallbackZip(isValidZipNum));
+
+//validation for cvv
+const cvvInput = document.querySelector('#cvv')
+cvvInput.placeholder = `123`
+let cvvValid = false;
+function validationCallbackCVV(isValidCVV) {
+  return e => {
+    const formInput = e.target.value;
+    const isValid = isValidCVV(formInput);
+    if (isValid) {
+      document.querySelector("[for=cvv]").textContent = `CVV: 😁`;
+      cvvValid = true;
+    } else {
+      document.querySelector("[for=cvv]").textContent = `CVV: 🤪 3 digits only`;
+      cvvValid = false;
+    }
+  }
+}
+
+function isValidCVV(cvvInput) {
+  return /\d{3}/.test(cvvInput);
+}
+
+cvvInput.addEventListener("change", validationCallbackCVV(isValidCVV));
+
+const validPaymentMethod = () => {
+  if (payWithCredit) {
+    if (creditValid && zipValid && cvvValid) {
+      return true
+    }
+  } else if ( payWithPaypal ){
+    return true
+  } else if ( payWithBitcoin ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const readyToSubmit = () => {
+  if ( nameValid && emailValid && activitySelected && validPaymentMethod() ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+const registerButton = document.querySelector('button')
+registerButton.textContent = `🤪 Form is Incomplete`;
+registerButton.disabled = true;
+const form = document.querySelector('form');
+form.addEventListener('change', () => {
+  if (readyToSubmit()) {
+    registerButton.disabled = false;
+    registerButton.textContent = `Register`;
+  } else {
+    registerButton.disabled = true;
+    registerButton.textContent = `🤪 Form is Incomplete`;
+  }
+})
+
